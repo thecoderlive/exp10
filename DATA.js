6534d2c4c93c249a1d7d8ba67988f6eacbb6f297
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-11-05 00:46:29 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "6df623ef-6871-4b7d-afdf-169568e740d1",
+    "home_feed": [
+        {
+            "id": "16bcdfaa-d474-4dbc-9499-0e7f9b6a8043",
+            "artist_name": "how",
+            "home_carousel": [
+                {
+                    "id": "43deaaf2-6f79-4c58-8764-6be9386045d2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/36.png"
+                },
+                {
+                    "id": "d3acc7a6-c4f3-4351-9c1f-7e95025227a6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/35.png"
+                },
+                {
+                    "id": "17c6d04c-9e84-4bd0-9f78-213fc7eeec9a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/344.png"
+                },
+                {
+                    "id": "65453663-d1ba-4d00-9f1a-5f347a38a429",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/131.png"
+                },
+                {
+                    "id": "431e7ada-bcbc-44fb-b632-a2828da3e727",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/246.png"
+                }
+            ],
+            "art_caption": "avoid",
+            "art_description": "Life TV shake boy billion general. Offer season yet test political son."
+        },
+        {
+            "id": "cca16c3e-65e7-4a88-83f5-af21df7c9eaa",
+            "artist_name": "brother",
+            "home_carousel": [
+                {
+                    "id": "594ce178-06fa-44ac-afad-26875875e0bd",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/259.png"
+                },
+                {
+                    "id": "f96dc9ee-8f00-4500-9caf-0d9c33261758",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/142.png"
+                },
+                {
+                    "id": "ff95b1e6-c4cf-4d89-ad2c-27cc2ae4efa5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/216.png"
+                },
+                {
+                    "id": "d26c8417-6d65-414a-bf60-b41e4056043d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/92.png"
+                },
+                {
+                    "id": "64051dad-4096-4e9f-ab36-7b20ddd16498",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/13.png"
+                }
+            ],
+            "art_caption": "television",
+            "art_description": "Discuss whole wonder family share. Federal different perhaps tree air civil marriage degree. Thing likely almost simply record."
+        },
+        {
+            "id": "e62c2b09-bbe4-412a-afe9-93a80ecaf18e",
+            "artist_name": "down",
+            "home_carousel": [
+                {
+                    "id": "370af9f0-e051-4778-8a38-fb5ab1f95c30",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/218.png"
+                },
+                {
+                    "id": "c7058b97-9da2-4b01-8265-e6a0add292f3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/144.png"
+                },
+                {
+                    "id": "69c107b9-431e-4c3b-a9c6-770cffc0c26b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/346.png"
+                },
+                {
+                    "id": "3a89a5ad-eb00-4e1b-bfe7-010ad498c857",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/44.png"
+                },
+                {
+                    "id": "27e2a810-5471-4c2d-a928-e710c22a0f8a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/9.png"
+                }
+            ],
+            "art_caption": "official",
+            "art_description": "Agreement lot whole executive good add. Various chair color. Small suffer race."
+        },
+        {
+            "id": "30b1f4ca-5e6c-4c51-9345-3f26099e2b02",
+            "artist_name": "together",
+            "home_carousel": [
+                {
+                    "id": "44a572a4-8af5-4cbe-8342-802eb329aed7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/6.png"
+                },
+                {
+                    "id": "bcc1d8f3-f5df-4d5f-9bdd-64f23e8d57e0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/207.png"
+                },
+                {
+                    "id": "2dc12dd9-ae35-4a47-9726-c7d1550e651b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/306.png"
+                },
+                {
+                    "id": "975fe1b1-bd4b-4b19-bb61-7f8acf7be0a4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/280.png"
+                },
+                {
+                    "id": "63e1f51f-8922-4e95-aa19-2b0d462519a5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/127.png"
+                }
+            ],
+            "art_caption": "knowledge",
+            "art_description": "Career ground once alone. Summer fall speech difference money six. News mission at visit six."
+        },
+        {
+            "id": "e75b7609-63f8-4f6d-b046-b5290f5b376d",
+            "artist_name": "conference",
+            "home_carousel": [
+                {
+                    "id": "86eedb16-8c89-45b6-a277-716b066481c6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/322.png"
+                },
+                {
+                    "id": "e4aeabf4-155f-41d6-91f1-721af8336717",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/259.png"
+                },
+                {
+                    "id": "bae780e5-f0c2-4661-bf5c-45f8aad93e17",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/177.png"
+                },
+                {
+                    "id": "05ab7b5c-845f-4a65-ae26-4b2b542b3838",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/176.png"
+                },
+                {
+                    "id": "c1596784-61c4-49d9-ad78-73c8aeb97405",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/196.png"
+                }
+            ],
+            "art_caption": "religious",
+            "art_description": "Rate short economy fear deal what own plan."
+        }
+    ]
+}
